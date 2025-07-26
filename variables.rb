57ffd954c838df3c725f3817e855d77f48900060
Abhijeet Singh <inabhijeet241107@gmail.com>
2025-07-26 23:21:14 +0530

    Learnig ruby

--- variables.rb
+++ variables.rb
@@ -1,1 +1,12 @@
-puts ""
+$global_var = "I'm global"
+
+class Example
+  @@class_var = "I'm a class variable"
+
+  def show
+    local_var = "I'm local"
+    puts local_var
+    puts @instance_var
+    puts @@class_var
+    puts $global_var
+  end
